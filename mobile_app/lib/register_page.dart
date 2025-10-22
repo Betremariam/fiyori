9a6392781aca32645725b953883ac554e4c314b8
Betremariam <yihenewbmariam@gmail.com>
2025-10-21 20:55:44 -0500

    the ui for register page created

---
+++ mobile_app/lib/register_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
